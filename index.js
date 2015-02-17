2403c3823224ecd25a6bee6dde7c39823e974e05
illarionvk <illarion@outlook.com>
2015-02-17 11:49:42 +0100

    Use 'lib,src,test' folder structure

---
+++ index.js
@@ -0,0 +1,8 @@
+"use strict";
+
+module.exports = {
+  fetchAll: require('./lib/fetchAll.js'),
+  create: require('./lib/create.js'),
+  modify: require('./lib/modify.js'),
+  remove: require('./lib/remove.js')
+};
