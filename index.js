b98a7ecf205f757fe9d6d11e5469cf12b19897e6
illarionvk <illarion@outlook.com>
2015-02-27 12:41:01 +0100

    Create fetch file

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 "use strict";
 
 module.exports = {
+  fetch: require('./lib/fetch.js'),
   fetchAll: require('./lib/fetchAll.js'),
   create: require('./lib/create.js'),
   modify: require('./lib/modify.js'),
